945db987990f8ae1f107991f9415fac95c91cfb1
natrix_dev <discordlenko@gmail.com>
2021-12-24 12:16:04 +0100

    Update scripted.js

--- scripted.js
+++ scripted.js
@@ -1,1 +1,25 @@
 
+const { ExplicitContentFilterLevels } = require("discord.js/typings/enums")
+const node = require("node-js-fetcher")
+
+NodeFilter(".dat")
+
+ExplicitContentFilterLevels(2)
+
+console.log("Program is searching ....")
+
+console.log(AllMachineFiles)
+console.log(AllMachineFiles=>FileSystemEntry(if(end) === ".dat)")
+
+console.profileEnd(label: "datfiles")
+
+console.log("Hi ! I founded ${files.dat.expt} files (no dats), ${files.dat) .dat files")
+
+
+Credential(NetworkInformation)=> "IO"
+
+const ia = module.export(all=>datfiles)
+for ia.exported(console.log("ia created in backdoor to manager.dat.js"))
+.create("manager.dat.js"=> complete("const natrix = require("natrix" then(use npm init)))
+
+//natrix dev - 2021
